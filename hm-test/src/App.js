13d1f8b914cf0bc2f933b9570dbb44d27aa6389c
MartinFunkquist <martin.funkquist@gmail.com>
2019-09-26 11:50:31 +0200

    Fixed the search task

---
+++ hm-test/src/App.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import './App.css';
+
+import SearchBar from './components/SearchBar';
+import SearchHistory from './components/SearchHistory';
+
+class App extends React.Component {
+  
+  state = {
+    searchHistory: []
+  }
+
+  addSearchToHistory = search => {
+    let newSearchHistory = this.state.searchHistory;
+    newSearchHistory.push(search);
+    this.setState({ searchHistory: newSearchHistory });
+  }
+
+  clearSearchHistory = () => {
+    this.setState({ searchHistory: [] });
+  }
+
+  removeSearchHistoryEntry = index => {
+    let newSearchHistory = this.state.searchHistory;
+    newSearchHistory.splice(index, 1);
+    this.setState({ searchHistory: newSearchHistory });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <SearchBar addSearchToHistory={this.addSearchToHistory} />
+        <SearchHistory 
+          searchHistory={this.state.searchHistory}
+          clearSearchHistory={this.clearSearchHistory}
+          removeSearchHistoryEntry={this.removeSearchHistoryEntry}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ hm-test/src/components/SearchHistory/index.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+class SearchHistory extends React.Component {
+
+	render() {
+
+		const { searchHistory } = this.props;
+
+		let historicalSearches;
+
+		if(searchHistory) {
+			historicalSearches = searchHistory.map((search, index) => {
+				return (
+					<li key={index}>
+						<span className="history-search-text">{search.text}</span>
+						<div>
+							<time>{formatDate(search.date)}</time>
+							<button 
+								className="link-button remove-btn"
+								onClick={(index) => this.props.removeSearchHistoryEntry(index)}>X</button>
+						</div>
+					</li>
+				)
+			})
+		}
+		return (
+			<>
+				<div className="search-history-top">
+					<h2>Search History</h2>
+					<button 
+						className="link-button"
+						onClick={this.props.clearSearchHistory}>
+						Clear search history
+					</button>
+				</div>
+				<ul className="search-history">
+					{historicalSearches}
+				</ul>
+			</>
+		)
+	}
+}
+
+function formatDate(date) {
+	
+	let hours = date.getHours();
+	let minutes = date.getMinutes();
+	
+	const ampm = hours >= 12 ? 'PM' : 'AM';
+	
+	hours = hours % 12;
+	minutes = minutes < 10 ? '0'+minutes : minutes;
+
+	return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}, ${hours}:${minutes} ${ampm}`; 
+}
+
+export default SearchHistory;
